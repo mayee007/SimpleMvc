0e8e7f1311546b81d02aea8234dccd470c962e7e
mayee007 <yelekeri@gmail.com>
2019-07-17 06:26:44 -0400

    details for person model

--- Controllers/Person2Controller.cs
+++ Controllers/Person2Controller.cs
@@ -3,17 +3,21 @@ using System.Text.Encodings.Web;
 using SimpleMvc.Models; 
 using SimpleMvc.Service; 
 using System.Linq;
-
+using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 
 namespace SimpleMvc.Controllers
 {
     public class Person2Controller : Controller
     {
         // 
-        // GET: /Person/
+        // GET: /Person2/
         public PersonDbContext context; 
 
-        // This controller needs PersonService to create the object 
+        // This controller needs Database context to create the object 
         public Person2Controller(PersonDbContext context) 
         {
             this.context = context; 
@@ -22,11 +26,29 @@ namespace SimpleMvc.Controllers
         public IActionResult Index()
         {
             // since the view name is specified as "Person", expected file name in the method name "Person.cshtml"
-            //return View(); 
+            // get all Person information from database  
             var persons = from p in context.persons
             orderby p.Id
             select p; 
             return View(persons);
+        }
+
+        // GET: Movies/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var person = await context.persons
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            return View(person);
         }
     }
 }
